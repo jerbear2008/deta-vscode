629e343c0ae12c4d69d7de9df9a2b528579d8f07
jerbear4328 <iamjerbear4328@gmail.com>
2023-02-06 22:03:31 -0800

    switch to custom engine

--- vscode/build.sh
+++ vscode/build.sh
@@ -3,6 +3,8 @@
 yarn
 yarn download-builtin-extensions
 # bash scripts/code-web.sh --help
-jq 'del(.["dependencies","devDependencies"]) | del(.scripts.postinstall) | .scripts.preinstall |= "node installError.js"' package.json > packageNew.json
-mv packageNew.json package.json
-cat package.json # debug
+# jq 'del(.["dependencies","devDependencies"]) | del(.scripts.postinstall) | .scripts.preinstall |= "node installError.js"' package.json > packageNew.json
+# mv packageNew.json package.json
+# cat package.json # debug
+wget https://nodejs.org/dist/v19.6.0/node-v19.6.0-linux-x64.tar.xz -O node.tar.xz
+tar -xf node.tar.xz -C node
