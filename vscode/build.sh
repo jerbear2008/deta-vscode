75f29b79ff7d42fdd5a7a963047328698fa88eef
jerbear4328 <iamjerbear4328@gmail.com>
2023-02-06 20:25:47 -0800

    upgrade build.sh

--- vscode/build.sh
+++ vscode/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Makes sure that code-web.js is ready to run directly
-bash scripts/code-web.sh --help
+# Makes sure that code-web.js is ready to run directly by deta
+yarn
+yarn download-builtin-extensions
+# bash scripts/code-web.sh --help
+jq 'del(.dependencies) | del(.devDependencies)' package.json > packageNew.json
+mv packageNew.json package.json
