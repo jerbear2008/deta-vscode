86f5c26db31af287dcd26861b8de498e1a388a00
jerbear4328 <iamjerbear4328@gmail.com>
2023-02-24 16:34:17 -0800

    now i know how, wip

--- vscode/build.sh
+++ vscode/build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Makes sure that code-web.js is ready to run directly by deta
 yarn
-yarn download-builtin-extensions
-# bash scripts/code-web.sh --help
-# jq 'del(.["dependencies","devDependencies"]) | del(.scripts.postinstall) | .scripts.preinstall |= "node installError.js"' package.json > packageNew.json
-# mv packageNew.json package.json
-# cat package.json # debug
+yarn gulp vscode-web-min
