436132fb129d5db040a95c9558bccb4be983088a
jerbear4328 <iamjerbear4328@gmail.com>
2023-03-03 20:52:49 -0800

    add memfs and put build script in own file

---
+++ build.sh
@@ -0,0 +1,16 @@
+# install and build frontend and extensions
+cd frontend
+
+# Install dependencies
+npm i
+
+# Move dependencies to the right spots
+split -C 3MB -d node_modules/vscode-web/dist/out/vs/workbench/workbench.web.main.js node_modules/vscode-web/dist/out/vs/workbench/workbench.web.main.js.part
+mkdir vscode-web
+cp -r ./node_modules/vscode-web/dist/* ./vscode-web
+cp ./node_modules/vscode-web/dist/{code-192.png,code-512.png,favicon.ico,manifest.json} .
+
+# Build extensions
+cd extensions/memfs
+npm i
+npm run build
